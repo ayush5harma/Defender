2976d5501d9ee4cf3fbb31ce16c3c71e24b29fea
Ayush Kumar Sharma <47772616+ayuu5h@users.noreply.github.com>
2020-02-07 10:15:17 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,23 @@
+import logging as log
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+def build_model(is_train):  
+    
+    n_inputs = 486
+    log.info("building model for %s ..." % 'training' if is_train else 'evaluation')
+    if is_train:
+        return Sequential([
+            Dense(70, input_shape=(n_inputs,), activation='relu'),
+            Dropout(0.3),
+            Dense(70, activation='relu'),
+            Dropout(0.3),
+            Dense(2, activation='softmax')
+        ])
+    else:
+        return Sequential([
+            Dense(70, input_shape=(n_inputs,), activation='relu'),
+            Dense(70, activation='relu'),
+            Dense(2, activation='softmax')
+        ])
